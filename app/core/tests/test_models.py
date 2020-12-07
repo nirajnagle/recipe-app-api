0bc813dacaf4db7510910e6835416bf2102497e4
Niraj Nagle <niraj@Niraj-MacBook.local>
2020-12-07 16:10:38 -0500

    Add tags api.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,10 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
 
+def sample_user(email='test@londonappdev.com', password='testpass'):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
 
 class ModelTests(TestCase):
     
@@ -15,21 +19,18 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
     
-    
     def test_new_user_email_normalized(self):
         """ Test the email for new user is normalized."""
         email = 'test@GMAil.COM'
         user = get_user_model().objects.create_user(email,'test123')
         
         self.assertEqual(user.email, email.lower())
-    
-    
+        
     def test_new_user_invalid_email(self):
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None,'test1234')
-    
-    
+        
     def test_create_new_superuser(self):
         """Creating a new super user"""
         user = get_user_model().objects.create_superuser(
@@ -38,4 +39,13 @@ class ModelTests(TestCase):
         )
         
         self.assertTrue(user.is_superuser)
-        self.assertTrue(user.is_staff)                
+        self.assertTrue(user.is_staff)
+    
+    def test_tag_str(self):
+        """Test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+
+        self.assertEqual(str(tag), tag.name)                    
